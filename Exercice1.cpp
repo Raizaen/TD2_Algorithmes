8740ba90046dcda8b8de9bb2fea342bd13e47d9c
Raizaen <31708305+Raizaen@users.noreply.github.com>
2017-09-22 08:13:24 +0200

    Cours du 22/09

---
+++ Exercice1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, fac(1);
+    cout << "Enter un entier pour calculer sa factorielle : " << endl;
+    cin >> n;
+    for(int i(1);i<n+1;i++)
+    {
+        fac = fac * i;
+    }
+    cout << "La factorielle de " << n << " est " << facto << std::endl;
+    return 0;
+}
